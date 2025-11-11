f5b587e52b17078c5edbafeedd01eda904969944
Max <mail@ru>
2025-11-11 22:56:17 +0300

    [insert endpoint added]

---
+++ internal/findcsv/findcsv.go
@@ -0,0 +1,32 @@
+package findcsv
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func FindAnyCSV(rootDir string) (string, error) {
+	dirEntries, err := os.ReadDir(rootDir)
+	if err != nil {
+		return "", err
+	}
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			folderPath := filepath.Join(rootDir, entry.Name())
+			files, err := os.ReadDir(folderPath)
+			if err != nil {
+				return "", err
+			}
+			for _, subentry := range files {
+				if !subentry.IsDir() && strings.HasSuffix(subentry.Name(), ".csv") {
+
+					return filepath.Join(folderPath, subentry.Name()), nil
+				}
+			}
+		} else if strings.HasSuffix(entry.Name(), ".csv") {
+			return filepath.Join(rootDir, entry.Name()), nil
+		}
+	}
+	return "", os.ErrNotExist
+}
--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"context"
+	"encoding/csv"
+	"encoding/json"
+
+	//"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	//"math/rand"
 	//"strconv"
 	//"encoding/json"
 
-	//"bytes"
-	//"io"
-
 	"itmo-devops-sem1-project-template/internal/config"
 	database "itmo-devops-sem1-project-template/internal/db"
-	//"itmo-devops-sem1-project-template/internal/tools"
+	findcsv "itmo-devops-sem1-project-template/internal/findcsv"
+	unpackage "itmo-devops-sem1-project-template/internal/tools"
 )
 
 const (
@@ -23,11 +28,11 @@ const (
 )
 
 type InsertResponse struct {
-	total_count      int `yaml: TotalCount`
-	duplicates_count int `yaml: DuplicatesCount`
-	total_items      int `yaml: DuplicatesCount`
-	total_categories int `yaml: TotalCategories`
-	total_price      int `yaml: TotalPrice`
+	TotalCount      int `yaml:"total_count"`
+	DuplicatesCount int `yaml:"duplicates_count"`
+	TotalItems      int `yaml:"total_items"`
+	TotalCategories int `yaml:"total_categories"`
+	TotalPrice      int `yaml:"total_price"`
 }
 
 func main() {
@@ -37,9 +42,7 @@ func main() {
 	log.Info("got config", slog.String("env", cfg.Env))
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Go!")
-	})
+	mux.HandleFunc("/api/v0/prices", UploadOnServer)
 
 	// получение данных из конфига
 	srv := &http.Server{
@@ -76,4 +79,125 @@ func setupLogger(env string) *slog.Logger {
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	}
 	return log
+}
+
+// ручка на отправку файлов
+func UploadOnServer(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	//считываем параметры из запроса
+	fileType := r.URL.Query().Get("type")
+
+	//определяем тип передаваемого архива
+	switch fileType {
+	case "tar":
+		err := unpackage.Untar(r.Body, "./tmp/extracted")
+		if err != nil {
+			http.Error(w, "Ошибка распаковки: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	default:
+		buf, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read body: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		readerAt := bytes.NewReader(buf)
+		size := int64(len(buf))
+		err = unpackage.Unzip(readerAt, size, "./tmp/extracted")
+		if err != nil {
+			http.Error(w, "Ошибка распаковки: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	//переопределяем счетчики
+	total_count := 0
+	duplicates_count := 0
+	total_items := 0
+	total_categories := 0
+	var total_price int
+
+	// находим файл
+	csvpath, error := findcsv.FindAnyCSV("./tmp/extracted")
+	if error != nil {
+		http.Error(w, "Ошибка поиска csv файла в архиве: "+error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// подключаемся к бд
+	db, error := database.СonnectDB()
+	if error != nil {
+		http.Error(w, "Не удалось подключиться к базе данных: "+error.Error(), http.StatusInternalServerError)
+	}
+	defer db.Close()
+
+	// вставка в cсожержимого архива в бд с подсчетом по условию
+	f, err := os.Open(csvpath)
+	if err != nil {
+		http.Error(w, "Не удалось прочитать файл из архива(1): "+err.Error(), http.StatusInternalServerError)
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		http.Error(w, "Не удалось прочитать файл из архива(2): "+err.Error(), http.StatusInternalServerError)
+	}
+
+	ctx := context.Background()
+
+	// логика набивания данных
+	var inserted bool
+	categories := make(map[string]struct{})
+	for _, rec := range records {
+		err := db.QueryRow(ctx, `
+			INSERT INTO prices (id, name, category, price, create_date)
+			VALUES ($1, $2, $3, $4, $5)
+			ON CONFLICT (id) DO UPDATE SET
+				name = EXCLUDED.name,
+				category = EXCLUDED.category,
+				price = EXCLUDED.price,
+				create_date = EXCLUDED.create_date
+			RETURNING (xmax = 0) AS inserted;
+		`, rec[0], rec[1], rec[2], rec[3], rec[4]).Scan(&inserted)
+		if err != nil {
+
+		}
+		if inserted {
+			total_items++
+		} else {
+			duplicates_count++
+		}
+		total_count++
+		if _, exists := categories[rec[1]]; !exists {
+			categories[rec[1]] = struct{}{}
+			total_categories++
+		}
+	}
+
+	// подсчет тотальной цены
+	_ = db.QueryRow(ctx, "SELECT SUM(price) FROM prices").Scan(&total_price)
+
+	// очистка директории с архивом
+	entries, _ := os.ReadDir("./tmp/extracted")
+	for _, entry := range entries {
+		_ = os.RemoveAll(filepath.Join("./tmp/extracted", entry.Name()))
+	}
+	// создание ответа
+	body, _ := json.Marshal(&InsertResponse{
+		TotalCount:      total_count,
+		DuplicatesCount: duplicates_count,
+		TotalItems:      total_items,
+		TotalCategories: total_categories,
+		TotalPrice:      total_price,
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }

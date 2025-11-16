3d7bf6e55762c0e0633d17ee9be322de82f9c4d7
Max <mail@ru>
2025-11-16 17:02:40 +0300

    [run.sh changes]

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 
+scp docker-compose.yml maxim@$(HOST_IP):/
+ssh -l maxim $(HOST_IP)
+docker compose up -d
 
-apt install golang-migrate # добавить migrate в 
-migrate -path=./migrations -database "postgresql://validator:val1dat0r@postgres:5432/project-sem-1?sslmode=disable" -verbose up # миграции бд
-go run insertInDB/insert.go # вставка данных в таблицу
+docker compose exec go-server apt install golang-migrate # добавить migrate в 
+docker compose exec go-server migrate -path=./migrations -database "postgresql://validator:val1dat0r@postgres:5432/project-sem-1?sslmode=disable" -verbose up # миграции бд
+docker compose exec go-server  go run insertInDB/insert.go # вставка данных в таблицу
 

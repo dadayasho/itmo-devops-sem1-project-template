6902adc0ab62da8e18adbcd0e78e096b3abe94ad
Max <mail@ru>
2025-11-18 01:19:12 +0300

    [test workflow]

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd terraform
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
 echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
 echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
 terraform init -reconfigure

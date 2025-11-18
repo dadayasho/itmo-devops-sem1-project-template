21ba28e6e9ff0437644484112e4159c94e6ae140
Max <mail@ru>
2025-11-18 13:29:24 +0300

    [run.sh fixes]

--- scripts/run.sh
+++ scripts/run.sh
@@ -45,8 +45,8 @@ scp ../docker-compose.yml maxim@${HOST_IP}:/home/maxim/docker-compose.yml
 # ====== Поднимаем бэкэнд ======
 ssh -o StrictHostKeyChecking=no -l maxim ${HOST_IP} "
   cd /home/maxim
-  docker compose up -d
-  docker compose exec backend apt install golang-migrate
-  docker compose exec backend migrate -path=./migrations -database "postgresql://validator:val1dat0r@postgres:5432/project-sem-1?sslmode=disable" -verbose up 
-  docker compose exec backend  go run insertInDB/insert.go 
+  sudo docker compose up -d
+  sudo docker compose exec backend apt install golang-migrate
+  sudo docker compose exec backend migrate -path=./migrations -database "postgresql://validator:val1dat0r@postgres:5432/project-sem-1?sslmode=disable" -verbose up 
+  sudo docker compose exec backend  go run insertInDB/insert.go 
 "

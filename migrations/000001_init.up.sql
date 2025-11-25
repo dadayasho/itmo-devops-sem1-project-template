834f28c4daa8ce48f9541e1f69060029d32ba7d8
maxim <maxim@ubuntu.myguest.virtualbox.org>
2025-11-25 13:45:16 +0300

    [fix in table]

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS prices(
     name TEXT NOT NULL, 
     category TEXT NOT NULL, 
     price NUMERIC(10, 2) NOT NULL, 
-    create_date DATE NOT NULL
+    create_date DATE NOT NULL,
     CONSTRAINT unique_price_entry UNIQUE (name, category, price, create_date)
 );
